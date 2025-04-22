899bcfe57e0343f886046c8bf7e6f4849761ff3c
santhosh <santhosh.003z@gmail.com>
2025-04-23 00:11:21 +0530

    feat: created app structure

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element="" />
+      <Route path="/room" element="">
+        <Route index element="" />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
