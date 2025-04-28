8ca0b7d4ec67219fd95bdcdeac7da219e746d42b
santhosh <santhosh.003z@gmail.com>
2025-04-28 23:50:18 +0530

    feat: moved message input to separate component

---
+++ src/utils/TextInput.tsx
@@ -0,0 +1,75 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
+interface inputData {
+  message: string;
+}
+
+const schema = z.object({
+  message: z.string().min(1),
+});
+
+function TextInput({
+  receiver,
+  setRefresh,
+}: {
+  receiver: string;
+  setRefresh: Function;
+}) {
+  const { register, handleSubmit } = useForm({ resolver: zodResolver(schema) });
+  const url = "http://localhost:3000";
+  const token = localStorage.getItem("authToken");
+  const Authorization = `Bearer ${token}`;
+  const header = {
+    headers: { Authorization },
+  };
+
+  function sendMessage(data: inputData) {
+    const split = receiver.split(" ");
+    let newData = {};
+    let newURL = url;
+
+    if (split.length === 1) {
+      newData = {
+        message: data.message,
+        receiverId: receiver,
+      };
+      newURL = url + "/message";
+    } else {
+      newData = {
+        message: data.message,
+        receiverId: split[1],
+      };
+      newURL = url + "/grp-msg";
+    }
+
+    axios
+      .post(newURL, newData, header)
+      .then((res) => {
+        const num = Math.floor(Math.random() * 100);
+        setRefresh(num);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
+
+  return (
+    <form className="message relative" onSubmit={handleSubmit(sendMessage)}>
+      <input
+        {...register("message")}
+        type="text"
+        name="message"
+        id="message"
+        className="rounded-4xl border-1 border-(--text-gray) w-full px-3 py-2 outline-none"
+      />
+      <button className="w-[30px] h-[30px] absolute top-[5px] right-[5px] cursor-pointer">
+        <img src="/assets/send.svg" alt="send" />
+      </button>
+    </form>
+  );
+}
+
+export default TextInput;
